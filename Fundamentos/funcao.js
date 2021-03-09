8ff325bc4fba136c998cb8822bd61ec9275b26ea
DavidPadilha95 <david.padilha@hotmail.com>
2021-03-09 17:32:45 -0300

    Novos testes

---
+++ Fundamentos/funcao.js
@@ -0,0 +1,38 @@
+
+// Função é um trecho de codigo que pode ser usado no codigo ao ser chamado
+console.log(typeof Object)
+
+class Produto {}
+console.log(typeof Produto)
+
+// Função sen retorno
+
+function imprimirSoma(a, b) {
+    console.log(`o valor da soma é ${a+b}`)
+}
+
+imprimirSoma(2, 3)
+
+// Função com retorno
+
+function soma(a,b=1) {
+    return a + b
+}
+console.log (soma(2,5))
+console.log (soma(2)) // o resutado sera 3 porque eu defini o b como 1
+
+// Armazenando um função em uma variavel
+const imprimirAdicao = function (a ,b) {
+    console.log( a+b)
+}
+imprimirAdicao(2,3)
+
+// Armazenando um função arrow em uma variavel
+const add = (a, b) => {
+    return a+b
+}
+console.log(4+9)
+
+// Retorno implicito
+const subtracao = (a, b) => a - b
+console.log(subtracao(2,3))
---
+++ Fundamentos/nullUndefined.js
@@ -0,0 +1,5 @@
+let valor //Dessa forma o valor será undefined, porque ainda anao foi atribuido um valor a essa variavel
+console.log(valor)
+
+valor = null // Ausencia de valor //Usar null para zerar o valor de uma variavel
+console.log(valor)
